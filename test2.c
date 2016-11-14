59fcabdd7bda9cad1e2e2ebf0159fc6171938d23
Bartosz Chmielewski <revanetv@gmail.com>
2016-11-14 00:49:36 +0000

    new

---
+++ test2.c
@@ -0,0 +1,20 @@
+/*
+==========
+== To proste! =
+==========
+    A jak to zrobic?
+Koniec :)
+*/
+
+#include<stdio.h>
+#include<cstdio>
+#include <iostream>
+
+int main()
+{
+  printf("========\n== To proste! ==\n ========");
+  printf("\t\nA jak to zrobic?");
+  std::cout<< "\nKoniec :)\n"<<"\ngruszki ciasta i wypieki\n";
+
+  return 0;
+}
